eaa1b9bc76bb3622b9405b49fce25494cc421372
weirdosu <sudianyuan@outlook.com>
2019-04-06 12:25:05 +0800

    增加abc中的具体内容

--- src/main/java/com/sdy/mvc01/controller/abc.java
+++ src/main/java/com/sdy/mvc01/controller/abc.java
@@ -5,4 +5,30 @@ package com.sdy.mvc01.controller;
  * @data 2019/4/6 12:20
  */
 public class abc {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "abc{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
